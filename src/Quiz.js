c00021e29a8a6bd295ef4e5f82d0cfac68b8a0e7
Chaithanya Kumar <68162279+Chaithanyac155@users.noreply.github.com>
2022-03-23 10:33:05 +0530

    modified

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,7 +1,169 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { data } from "./data";
 
 const Quiz = () => {
-  return <>quiz app</>;
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [myanswer, setMyAnswer] = useState("");
+  const [finish, setFinish] = useState(false);
+  const [clickAnswer, setClickAnswer] = useState(false);
+  const [show, setShow] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const selectAnswerRadio = (e) => {
+    setMyAnswer(e.currentTarget.value);
+    setClickAnswer(true);
+    setShow(false);
+  };
+
+  const showAnswer = () => {
+    setShow(!show);
+    // setFinish(false);
+  };
+  const checkCorrectAnswer = () => {
+    if (myanswer === data[currentQuestion].answer) {
+      setScore(score + 1);
+    }
+  };
+  const reset = () => {
+    setMyAnswer("");
+    setClickAnswer(false);
+    setShow(false);
+  };
+
+  const handleSubmit = () => {
+    if (currentQuestion === data.length - 1) {
+      setFinish(true);
+      setClickAnswer(true);
+    }
+  };
+
+  const handleRetakeQuiz = () => {
+    setCurrentQuestion(0);
+    setFinish(false);
+    setMyAnswer("");
+    setClickAnswer(false);
+    setScore(0);
+  };
+  if (finish) {
+    return (
+      <div className="container finish">
+        <h4
+          style={{ textAlign: "center", color: "" }}
+          className="p-2 mb-3 bg-info text-white rounded"
+        >
+          Quiz
+        </h4>
+        <h5>Quiz Over!</h5>
+        <h5>{`Your Score is ${score}/${data.length}`}</h5>
+        <button
+          type="button"
+          className="btn btn-warning"
+          onClick={handleRetakeQuiz}
+        >
+          Retake Quiz
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container">
+        <h4
+          style={{ textAlign: "center", color: "" }}
+          className="p-2 mb-3 bg-info text-white rounded"
+        >
+          Quiz
+        </h4>
+        <h4 className="question">
+          {`${data[currentQuestion].id}) ${data[currentQuestion].question}`}
+        </h4>
+        <p className="questions-count">{`${currentQuestion + 1}/${
+          data.length
+        }`}</p>
+        {data[currentQuestion].variants.map((variant, index) => (
+          <li key={index} className="answer-item">
+            <input
+              type="radio"
+              name="select-answer"
+              value={variant}
+              checked={myanswer === variant}
+              onChange={selectAnswerRadio}
+              className={`answer-item ${
+                myanswer === variant
+                  ? myanswer === data[currentQuestion].answer
+                    ? "correctAnswer"
+                    : "wrongAnswer"
+                  : null
+              }`}
+            />
+            <span className="ml-2" style={{ cursor: "default" }}>
+              {variant}
+            </span>
+            <span
+              className={` ${
+                myanswer === variant
+                  ? myanswer === data[currentQuestion].answer
+                    ? "correctCaption"
+                    : "wrongCaption"
+                  : null
+              }`}
+            >{`${
+              myanswer === variant
+                ? myanswer === data[currentQuestion].answer
+                  ? "Uho! right answer"
+                  : "sry! wrong answer"
+                : ""
+            }`}</span>
+          </li>
+        ))}
+        {clickAnswer && (
+          <button
+            type="button"
+            className="showAnswer"
+            onClick={() => showAnswer()}
+          >
+            Check your Answer Here!
+          </button>
+        )}
+        {show && (
+          <p className="actual-answer">
+            Correct Answer: <span> {data[currentQuestion].answer}</span>
+          </p>
+        )}
+
+        <div className="btn-prev-next">
+          <button
+            className="btn btn-secondary"
+            disabled={currentQuestion === 0}
+            onClick={() => {
+              setCurrentQuestion(currentQuestion - 1);
+              checkCorrectAnswer();
+            }}
+          >
+            PREV
+          </button>
+
+          {currentQuestion < data.length - 1 && (
+            <button
+              className="btn btn-primary"
+              disabled={currentQuestion === data.length - 1}
+              onClick={() => {
+                setCurrentQuestion(currentQuestion + 1);
+                checkCorrectAnswer();
+                reset();
+              }}
+            >
+              NEXT
+            </button>
+          )}
+          {currentQuestion === data.length - 1 && (
+            <button className="btn btn-success" onClick={() => handleSubmit()}>
+              SUBMIT
+            </button>
+          )}
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Quiz;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    id: "1",
+    question: "What is the capital of AndhraPradesh",
+    answer: "Amaravathi",
+    variants: ["Mumbai", "Amaravathi", "Patna", "Hyderabad"]
+  },
+  {
+    id: "2",
+    question: "What is the capital of Telangana",
+    answer: "Hyderabad",
+    variants: ["Patna", "Chennai", "Hyderabad", "Amaravathi"]
+  },
+  {
+    id: "3",
+    question: "What is the capital of Karnataka",
+    answer: "Bangalore",
+    variants: ["Bangalore", "Hyderabad", "Chennai", "Mumbai"]
+  },
+  {
+    id: "4",
+    question: "What is the capital of Goa",
+    answer: "Panaji",
+    variants: ["Chennai", "Shimla", "Ranchi", "Panaji"]
+  },
+  {
+    id: "5",
+    question: "What is the capital of Tamil Nadu",
+    answer: "Chennai",
+    variants: ["Mumbai", "Chennai", "Bangalore", "Hyderabad"]
+  }
+];
